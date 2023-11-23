2795cb139206ddf0d755a64435475531bd3968c6
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-11-23 23:20:34 +0900

    end chapter15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,6 +6,7 @@ import { sql } from "@vercel/postgres";
 import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
 
 const InvoiceSchema = z.object({
   id: z.string(),
@@ -154,4 +155,19 @@ export async function deleteInvoice (id: string) {
 
   // キャッシュクリアして、サーバーに新しくリクエストを作成
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData))
+  }
+  catch (error) {
+    if((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignin';
+    }
+    throw error;
+  }
 }
